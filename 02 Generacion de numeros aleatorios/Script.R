34e4c59a328c11aa8655391e61cab6bf638e2bd0
BernardoMB <bmondragonbrozon@gmail.com>
2018-09-06 11:45:56 -0500

    Me faltan la 4 y las preguntas 6-11

--- 02 Generacion de numeros aleatorios/Script.R
+++
@@ -1,8 +0,0 @@
-# Obtener la fecha actual
-Sys.time()
-
-# Fijar un valor semilla para generar los mismos numeros aleatorios
-set.seed(100)
-
-# Generar un vector con 50 numeros aleatorios distribuidos uniformemente
-runif(50)
